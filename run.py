7bd81ce120f4a8c36a766bac1e8931bbac624383
Alex Maras <alexmaras@gmail.com>
2020-08-21 10:40:03 +0800

    Finds all overdue items (via OVERDUE_HOURS_LIMIT env variable)

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import os
 import todoist
+import pytz
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 REQUIRED_ENV_VARIABLES = [
@@ -23,6 +25,7 @@ def main():
 
     autocomplete_label_id = find_autocomplete_label(api.state["labels"], os.getenv("AUTOCOMPLETE_LABEL"))
     autocomplete_items = find_autocomplete_items(api.state["items"], autocomplete_label_id)
+    overdue_items = find_overdue_items(autocomplete_items, os.getenv("OVERDUE_HOURS_LIMIT"), os.getenv("LOCAL_TIMEZONE"))
 
 def check_required_env():
     get_env_vars = lambda required_env: os.getenv(required_env)
@@ -37,5 +40,19 @@ def find_autocomplete_items(items, label):
     filter_by_label = lambda item: label in item["labels"]
     autocomplete_items = filter(filter_by_label, items)
     return autocomplete_items
+
+def find_overdue_items(items, overdue_limit, local_timezone):
+    overdue_items = []
+    for item in items:
+        item_date = item["due"]["date"]
+        date = item_date if len(item_date) == 19 else item_date[:-1]
+        tz = item["due"]["timezone"] or local_timezone
+
+        now = datetime.now(pytz.timezone(tz))
+        due_date = datetime.fromisoformat(date).astimezone(pytz.timezone(tz))
+        overdue_date = due_date + timedelta(hours=int(overdue_limit))
+        if now > overdue_date:
+            overdue_items.append(item)
+    return overdue_items
 
 main()
