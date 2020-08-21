d6520a8ca0167f5076e5438078d7f809ba6f664a
Alex Maras <alexmaras@gmail.com>
2020-08-21 23:21:58 +0800

    Close items instead of completing, and check on item{'checked']

--- run.py
+++ run.py
@@ -37,7 +37,7 @@ def main():
     )
 
     for item in overdue_items:
-        api.items.complete(item["id"])
+        api.items.close(item["id"])
 
     api.commit()
     print("Items completed")
@@ -52,7 +52,7 @@ def find_autocomplete_label(labels, required_label):
             return label["id"]
 
 def find_autocomplete_items(items, label):
-    filter_by_label = lambda item: label in item["labels"]
+    filter_by_label = lambda item: label in item["labels"] and item["checked"] == 0
     autocomplete_items = filter(filter_by_label, items)
     return autocomplete_items
 
